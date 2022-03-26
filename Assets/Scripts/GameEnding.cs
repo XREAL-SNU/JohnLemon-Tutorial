05dac28c71a9a9bb2bbfe7823abc0733bab4bb4e
HyunJi Lim <66664845+LimHyun-ji@users.noreply.github.com>
2022-03-26 23:29:47 +0900

    [Feat] Audio

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -10,7 +10,10 @@ public class GameEnding : MonoBehaviour
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
     public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource exitAudio;
+    public AudioSource caughtAudio;
 
+    bool m_HasAudioPlayed;
     bool m_IsPlayerCaught;
     bool m_IsPlayerAtExit;
     float m_Timer;
@@ -30,16 +33,21 @@ public class GameEnding : MonoBehaviour
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel(exitBackgroundImageCanvasGroup, false);
+            EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
         }
         else if(m_IsPlayerCaught)
         {
-            EndLevel(caughtBackgroundImageCanvasGroup, true);
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
         }
     }
 
-    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
     {
+        if(!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed=true;
+        }
         m_Timer += Time.deltaTime;
         imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     Animator m_Animator;
     Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
     public float turnSpeed =20f;
@@ -14,6 +15,7 @@ public class PlayerMovement : MonoBehaviour
     {
         m_Animator=GetComponent<Animator>();
         m_Rigidbody=GetComponent<Rigidbody>();
+        m_AudioSource=GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -32,6 +34,15 @@ public class PlayerMovement : MonoBehaviour
 
         m_Animator.SetBool("IsWalking", isWalking);
 
+        if(isWalking)
+        {
+            if(!m_AudioSource.isPlaying)
+                m_AudioSource.Play();
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
         //가는 방향으로 회전하기
         Vector3 desiredForward=Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed *Time.deltaTime, 0f);
         m_Rotation=Quaternion.LookRotation(desiredForward);
