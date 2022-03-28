f5c501b0033ca8e3a6f76b67382bf4b978727f81
Junhee Lee <heunghari@naver.com>
2022-03-28 18:15:58 +0900

    control movements

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-{
+{   
+    Animator m_Animator;
+    Vector3 m_Movement;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +14,15 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        float horizontal = input.GetAxis("Horizontal");
-        float vertical = input.GetAxis("Vertical");
+    {   
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
     }
 }
